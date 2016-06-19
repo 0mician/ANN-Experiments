42fa0151e1deb11ab54e50c18d3d56dcac8846a1
Milt0n <cedric.lood@gmail.com>
2016-06-19 13:22:59 +0200

    finished part on SOM

--- som_concentric_cylinders.m
+++ som_concentric_cylinders.m
@@ -1,38 +1,131 @@
-%% perform unsupervised learning with SOM  
-
 % Marco Signoretto, March 2011
-
-
-
-close all;
-clear all;
-clc;
+close all; clear all; clc;
 
 % first we generate data uniformely distributed within two
 % concentric cylinders
-
 X=2*(rand(5000,3)-.5);
 indx=(X(:,1).^2+X(:,2).^2<.6)&(X(:,1).^2+X(:,2).^2>.1);
 X=X(indx,:)';
 
 % we then initialize the SOM with hextop as topology function
 % and linkdist as distance function
-net = newsom(X,[5 5 5],'hextop','linkdist'); 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+nethex  = newsom(X,[5 5 5],'hextop','linkdist'); 
+netgrid = newsom(X,[5 5 5],'gridtop','linkdist'); 
+netrand = newsom(X,[5 5 5],'randtop','linkdist'); 
 
 % plot the data distribution with the prototypes of the untrained network
-figure;plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure('Color', [1 1 1]);
+subplot(3,4,1);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
 axis([-2 2 -2 2]);
 hold on
-plotsom(net.iw{1,1},net.layers{1}.distances)
+plotsom(nethex.iw{1,1},nethex.layers{1}.distances)
+title('Hexagonal topology initial','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+hold off
+
+subplot(3,4,5);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+axis([-2 2 -2 2]);
+hold on
+plotsom(netgrid.iw{1,1},netgrid.layers{1}.distances)
+title('Grid topology initial','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+hold off
+
+subplot(3,4,9);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+axis([-2 2 -2 2]);
+hold on
+plotsom(netrand.iw{1,1},netrand.layers{1}.distances)
+title('Random topology initial','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
 hold off
 
 % finally we train the network and see how their position changes
-net.trainParam.epochs = 1000;
-net = train(net,X);
-figure;plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Hexagonal topology
+nethex.trainParam.epochs = 1000;
+nethex = train(nethex,X);
+subplot(3,4,2);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
 axis([-1 1 -1 1]);
 hold on
-plotsom(net.iw{1,1},net.layers{1}.distances)
+plotsom(nethex.iw{1,1},nethex.layers{1}.distances)
+title('Trained (X,Y)','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+hold off
+subplot(3,4,3);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+axis([-1 1 -1 1]);
+hold on
+plotsom(nethex.iw{1,1},nethex.layers{1}.distances)
+title('Trained (X,Z)','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
 hold off
 
+% Grid topology
+netgrid.trainParam.epochs = 1000;
+netgrid = train(netgrid,X);
+subplot(3,4,6);
+title('Grid topology','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+axis([-1 1 -1 1]);
+hold on
+plotsom(netgrid.iw{1,1},netgrid.layers{1}.distances)
+title('Trained (X,Y)','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+hold off
+subplot(3,4,7);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+axis([-1 1 -1 1]);
+hold on
+plotsom(netgrid.iw{1,1},netgrid.layers{1}.distances)
+title('Trained (X,Z)','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+hold off
+
+% Random topology
+netrand.trainParam.epochs = 1000;
+netrand = train(netrand,X);
+subplot(3,4,10);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+axis([-1 1 -1 1]);
+hold on
+plotsom(netrand.iw{1,1},netrand.layers{1}.distances)
+title('Trained (X,Y)','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+hold off
+subplot(3,4,11);
+plot3(X(1,:),X(2,:),X(3,:),'.g','markersize',10);
+axis([-1 1 -1 1]);
+hold on
+plotsom(netrand.iw{1,1},netrand.layers{1}.distances)
+title('Trained (X,Z)','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+hold off
  
--- som_example_iris.m
+++ som_example_iris.m
@@ -1,9 +1,5 @@
-clear
-clc
-close all
+clear; clc; close all;
 
-%Load data
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 load fisheriris
 X = meas(:,1:end-1);
 true_labels = meas(:,end); 
@@ -15,24 +11,30 @@ for i=101:150
     temp(i)=3;
 end
 true_labels = temp;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Training the SOM
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%
 x_length = 3;
 y_length = 1;
 net = newsom(X',[y_length x_length],'gridtop');
-net.trainParam.epochs = 100;
+net.trainParam.epochs = 100000;
 net = train(net,X');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure('Color', [1 1 1]);
+scatter3(X(:,1),X(:,2),X(:,3), 20, true_labels)%,'.g','markersize',10);
+hold on
+plotsom(net.iw{1,1},net.layers{1}.distances)
+title('Iris dataset SOM','FontSize',18,'FontWeight', 'normal');
+xlabel('W(i,1)','FontSize',14);
+ylabel('W(i,2)','FontSize',14);
+zlabel('W(i,3)', 'Fontsize', 14);
+hold off
 
 % Assigning examples to clusters
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 outputs = sim(net,X');
 [~,assignment]  =  max(outputs);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Compare clusters with true labels
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [AR,RI,MI,HI]=unsupervised_randindex(assignment,true_labels');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
